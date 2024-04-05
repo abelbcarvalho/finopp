de6614163a3bd3720c0c57e560922cd96043bd74
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-05 13:27:10 -0300

    feat: inserted sql file with the tables

---
+++ fin_opp.sql
@@ -0,0 +1,27 @@
+create table if not exists Users
+(
+	id serial not null,
+	primary key(id),
+	name varchar(128) not null,
+	dateBorn date not null,
+	cpf varchar(12) unique not null,
+	username varchar(128) unique not null,
+	email varchar(255) unique,
+	passwd varchar(512) not null,
+	createAt timestamp not null,
+	updateAt timestamp
+);
+
+create table if not exists FinOpp
+(
+	id serial not null,
+	primary key(id),
+	userId int not null,
+	foreign key (userId) references Users(id),
+	description varchar(255) not null,
+	category varchar(64) not null,
+	typeOpp varchar(10) not null,
+	finMoney decimal(10, 2) not null,
+	createAt timestamp not null,
+	updateAt timestamp
+);
