afa995eac0e8c39fedd899634f56ef3bf9849a9f
Abel BC <160126794+abelbcarvalho@users.noreply.github.com>
2024-04-05 17:13:21 -0300

    feat: create database structure connection (#4)
    * feat: generic exception database
    
    * feat: database connection class created

---
+++ Src/Exceptions/ExceptionDatabase.cs
@@ -0,0 +1,11 @@
+namespace ExceptionDatabase
+{
+    public class DatabaseException : Exception
+    {
+        public DatabaseException()
+        {}
+
+        public DatabaseException(string message) : base(message)
+        {}
+    }
+}
