16a98780db16f7dff4a618499b3412d77eb4402f
Abel BC <160126794+abelbcarvalho@users.noreply.github.com>
2024-04-09 11:45:14 -0300

    feat: create interfaces for finopp service controller persistdb (#12)
    * feat: controller interface for finopp
    
    * feat: service interface for finopp
    
    * feat: persistdb interface for finopp
    
    * hotfix: methods update and finish

---
+++ Src/Services/IServiceFinOpp.cs
@@ -0,0 +1,19 @@
+using System.Numerics;
+using EnumFinType;
+using ModelFinOpp;
+
+namespace IServiceFinOpp
+{
+    public interface IFinOppService
+    {
+        abstract void CreateFinOpp(FinOpp finOpp);
+        abstract List<FinOpp> FindByDate(DateTime date, bool finish, BigInteger userId);
+        abstract List<FinOpp> FindByDateRange(DateTime firstDate, DateTime lastDate, bool finish, BigInteger userId);
+        abstract List<FinOpp> FindByMoneyGreaterThanEquals(decimal money, bool finish, BigInteger userId);
+        abstract List<FinOpp> FindByMoneyRange(decimal moneyFirst, decimal moneyLast, bool finish, BigInteger userId);
+        abstract List<FinOpp> FindByCategory(string category, bool finish, BigInteger userId);
+        abstract List<FinOpp> FindByFinType(FinTypeEnum finTypeEnum, bool finish, BigInteger userId);
+        abstract void UpdateFinOpp(FinOpp finOpp);
+        abstract void FinishFinOpp(BigInteger id);
+    }
+}
