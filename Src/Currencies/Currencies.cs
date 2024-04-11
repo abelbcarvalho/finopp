912f1ff6ba2999b43fd9220a2a8b5a91ff08cbd7
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-11 11:39:01 -0300

    feat: api url and a method to join them to currencies

--- Src/Currencies/Currencies.cs
+++ Src/Currencies/Currencies.cs
@@ -1,5 +1,12 @@
 namespace Currencies
 {
     public class FinanceCurrency
-    { }
+    {
+        private readonly string apiUrl = "http://economia.awesomeapi.com.br/json/last";
+
+        protected string JoinCurrenciesToUrl(string currencies)
+        {
+            return $"{this.apiUrl}/{currencies}";
+        }
+    }
 }
