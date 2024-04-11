e0abffcbaf3d52220721035c168b86804005f084
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-11 11:50:19 -0300

    feat: currencies methods from interface needs to be implemmented

--- Src/Currencies/Currencies.cs
+++ Src/Currencies/Currencies.cs
@@ -1,8 +1,41 @@
+using CurrencyDTO;
+using ICurrencies;
+
 namespace Currencies
 {
-    public class FinanceCurrency
+    public class FinanceCurrency : IFinanceCurrency
     {
         private readonly string apiUrl = "http://economia.awesomeapi.com.br/json/last";
+
+        public DTOCurrency BitcoinToReal(decimal bitcoin)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTOCurrency DollarToReal(decimal dollar)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTOCurrency EuroToReal(decimal euro)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTOCurrency RealToBitcoin(decimal real)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTOCurrency RealToDollar(decimal real)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DTOCurrency RealToEuro(decimal real)
+        {
+            throw new NotImplementedException();
+        }
 
         protected string JoinCurrenciesToUrl(string currencies)
         {
