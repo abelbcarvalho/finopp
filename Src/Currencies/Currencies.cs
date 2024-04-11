c4cef7def4fe38f77e7ed1ec03756ea113e71b2b
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-11 15:06:24 -0300

    feat: get the correct url for each query

--- Src/Currencies/Currencies.cs
+++ Src/Currencies/Currencies.cs
@@ -9,31 +9,43 @@ namespace Currencies
 
         public DTOCurrency BitcoinToReal(decimal bitcoin)
         {
+            string currency = this.JoinCurrenciesToUrl("BTC-BRL");
+
             throw new NotImplementedException();
         }
 
         public DTOCurrency DollarToReal(decimal dollar)
         {
+            string currency = this.JoinCurrenciesToUrl("USD-BRL");
+
             throw new NotImplementedException();
         }
 
         public DTOCurrency EuroToReal(decimal euro)
         {
+            string currency = this.JoinCurrenciesToUrl("EUR-BRL");
+
             throw new NotImplementedException();
         }
 
         public DTOCurrency RealToBitcoin(decimal real)
         {
+            string currency = this.JoinCurrenciesToUrl("BRL-BTC");
+
             throw new NotImplementedException();
         }
 
         public DTOCurrency RealToDollar(decimal real)
         {
+            string currency = this.JoinCurrenciesToUrl("BRL-USD");
+
             throw new NotImplementedException();
         }
 
         public DTOCurrency RealToEuro(decimal real)
         {
+            string currency = this.JoinCurrenciesToUrl("BRL-EUR");
+
             throw new NotImplementedException();
         }
 
