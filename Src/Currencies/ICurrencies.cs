02f49c5854c9e9fa5340a3d05d361ac82f8f6d9c
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-11 11:48:32 -0300

    feat: methods to be implemented to currencies class

--- Src/Currencies/ICurrencies.cs
+++ Src/Currencies/ICurrencies.cs
@@ -1,5 +1,14 @@
+using CurrencyDTO;
+
 namespace ICurrencies
 {
     public interface IFinanceCurrency
-    { }
+    {
+        abstract DTOCurrency DollarToReal(decimal dollar);
+        abstract DTOCurrency RealToDollar(decimal real);
+        abstract DTOCurrency EuroToReal(decimal euro);
+        abstract DTOCurrency RealToEuro(decimal real);
+        abstract DTOCurrency BitcoinToReal(decimal bitcoin);
+        abstract DTOCurrency RealToBitcoin(decimal real);
+    }
 }
