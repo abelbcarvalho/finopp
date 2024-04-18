e8001d90330599bd749199a86d21f164baf85bad
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-18 17:28:53 -0300

    feat: create user on database working

--- Src/PersistDB/User/PersistUser.cs
+++ Src/PersistDB/User/PersistUser.cs
@@ -1,4 +1,6 @@
+using System.Data;
 using System.Numerics;
+using Database;
 using IPersistUser;
 using UserLoginDTO;
 
@@ -8,7 +10,32 @@ namespace PersistUser
     {
         public async Task CreateUser(ModelUser.User user)
         {
-            throw new NotImplementedException();
+            string sql = "insert into Users (name,dateborn,cpf,username,email,passwd,active,createat,updateat) values (@name,@dateborn,@cpf,@username,@email,@passwd,@active,@createat,@updateat)";
+
+            user.CreateAt = DateTime.Now;
+
+            try
+            {
+                await DBConnection.GetConnectionAsync();
+
+                using var command = await DBConnection.CommandAsync(sql);
+
+                command.Parameters.AddWithValue("@name", user.Name);
+                command.Parameters.AddWithValue("@dateborn", user.DateBorn);
+                command.Parameters.AddWithValue("@cpf", user.Cpf);
+                command.Parameters.AddWithValue("@username", user.Username);
+                command.Parameters.AddWithValue("@email", user.Email ?? "");
+                command.Parameters.AddWithValue("@passwd", user.Password);
+                command.Parameters.AddWithValue("@active", user.Active);
+                command.Parameters.AddWithValue("@createat", user.CreateAt);
+                command.Parameters.AddWithValue("@updateat", user.UpdateAt ?? user.CreateAt);
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
 
         public async Task DisableUser(BigInteger id)
