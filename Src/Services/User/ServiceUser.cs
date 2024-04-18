fd897744bef89ce2477e872b1f1b25704e8096e5
Abel BC <160126794+abelbcarvalho@users.noreply.github.com>
2024-04-18 11:44:39 -0300

    feat: cehcker data on service (#17)
    * chore: files moved to their meaning on service and controller
    
    * feat: service user checker done

--- Src/Services/User/ServiceUser.cs
+++ Src/Services/User/ServiceUser.cs
@@ -3,6 +3,9 @@ using IServiceUser;
 using ModelUser;
 using PersistUser;
 using UserLoginDTO;
+using UtilCpf;
+using UtilEmail;
+using UtilUsername;
 
 namespace ServiceUser
 {
@@ -12,6 +15,24 @@ namespace ServiceUser
 
         public async Task CreateUser(User user)
         {
+            if (String.IsNullOrEmpty(user.Name))
+            {
+                throw new Exception("error: name can't be null or empty");
+            }
+
+            CpfUtil.CpfChecker(user.Cpf);
+            UsernameUtil.CheckUsername(user.Username);
+
+            if (user.Email != null)
+            {
+                EmailUtil.CheckEmailAddress(user.Email);
+            }
+
+            if (String.IsNullOrEmpty(user.Password))
+            {
+                throw new Exception("error: password can't be null or empty");
+            }
+
             await this.userPersistDB.CreateUser(user);
         }
 
