ecd05bee91826c2b83684932119ad45c2a322a09
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-18 10:49:58 -0300

    feat: async methods to be implemented on finopp persistdb

---
+++ Src/PersistDB/FinOpp/PersistFinOpp.cs
@@ -0,0 +1,55 @@
+using System.Numerics;
+using EnumFinType;
+using IPersistFinOpp;
+using ModelFinOpp;
+
+namespace PersistFinOpp
+{
+    public class FinOppPersistDB : IFinOppPersistDB
+    {
+        public async void CreateFinOpp(FinOpp finOpp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<FinOpp>> FindByCategory(string category, bool finish, BigInteger userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<FinOpp>> FindByDate(DateTime date, bool finish, BigInteger userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<FinOpp>> FindByDateRange(DateTime firstDate, DateTime lastDate, bool finish, BigInteger userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<FinOpp>> FindByFinType(FinTypeEnum finTypeEnum, bool finish, BigInteger userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<FinOpp>> FindByMoneyGreaterThanEquals(decimal money, bool finish, BigInteger userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<FinOpp>> FindByMoneyRange(decimal moneyFirst, decimal moneyLast, bool finish, BigInteger userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void FinishFinOpp(BigInteger id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async void UpdateFinOpp(FinOpp finOpp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
