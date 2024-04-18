19c60ad63bd531c6cc2341feb6b7e320599df4a2
abelbcarvalho <abelbcarvalho22@gmail.com>
2024-04-18 10:25:08 -0300

    refact: not void persist db interface to task

--- Src/PersistDB/User/IPersistUser.cs
+++ Src/PersistDB/User/IPersistUser.cs
@@ -7,8 +7,8 @@ namespace IPersistUser
     public interface IUserPersistDB
     {
         abstract void CreateUser(User user);
-        abstract User MakeLogin(DTOUserLogin userLogin);
-        abstract User RecoveryAccess(User user);
+        abstract Task<User> MakeLogin(DTOUserLogin userLogin);
+        abstract Task<User> RecoveryAccess(User user);
         abstract void DisableUser(BigInteger id);
         abstract void EnableUser(User user);
     }
