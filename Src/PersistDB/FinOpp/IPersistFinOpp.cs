a539da0fa42272ba6444f5bd2b0aee0803972412
Abel BC <160126794+abelbcarvalho@users.noreply.github.com>
2024-04-18 10:27:05 -0300

    refactor: transform methods to async (#14)
    * refact: not void controller interface to task
    
    * refact: not void service interface to task
    
    * refact: not void persist db interface to task

--- Src/PersistDB/FinOpp/IPersistFinOpp.cs
+++ Src/PersistDB/FinOpp/IPersistFinOpp.cs
@@ -7,12 +7,12 @@ namespace IPersistFinOpp
     public interface IFinOppPersistDB
     {
         abstract void CreateFinOpp(FinOpp finOpp);
-        abstract List<FinOpp> FindByDate(DateTime date, bool finish, BigInteger userId);
-        abstract List<FinOpp> FindByDateRange(DateTime firstDate, DateTime lastDate, bool finish, BigInteger userId);
-        abstract List<FinOpp> FindByMoneyGreaterThanEquals(decimal money, bool finish, BigInteger userId);
-        abstract List<FinOpp> FindByMoneyRange(decimal moneyFirst, decimal moneyLast, bool finish, BigInteger userId);
-        abstract List<FinOpp> FindByCategory(string category, bool finish, BigInteger userId);
-        abstract List<FinOpp> FindByFinType(FinTypeEnum finTypeEnum, bool finish, BigInteger userId);
+        abstract Task<List<FinOpp>> FindByDate(DateTime date, bool finish, BigInteger userId);
+        abstract Task<List<FinOpp>> FindByDateRange(DateTime firstDate, DateTime lastDate, bool finish, BigInteger userId);
+        abstract Task<List<FinOpp>> FindByMoneyGreaterThanEquals(decimal money, bool finish, BigInteger userId);
+        abstract Task<List<FinOpp>> FindByMoneyRange(decimal moneyFirst, decimal moneyLast, bool finish, BigInteger userId);
+        abstract Task<List<FinOpp>> FindByCategory(string category, bool finish, BigInteger userId);
+        abstract Task<List<FinOpp>> FindByFinType(FinTypeEnum finTypeEnum, bool finish, BigInteger userId);
         abstract void UpdateFinOpp(FinOpp finOpp);
         abstract void FinishFinOpp(BigInteger id);
     }
